0fa970bcf50e40cb3adac4a40f906daedff64d48
SAGAR T <ssagu6661@gmail.com>
2024-11-04 18:58:50 +0530

    Add files via upload

---
+++ engg_program1.c
@@ -0,0 +1,21 @@
+/*Print a number 1 to 10 without using loops*/
+
+#include<stdio.h>
+ 
+int main()
+{
+//     int i = 0;
+// sagar:
+//     i = i + 1;
+//     printf("%d\n", i);
+ 
+//     if (i < 10)
+//         goto sagar;
+int i = 1;
+while (i<11)
+{
+  printf("%d\n", i);
+ i++;
+}
+    return 0;
+}
