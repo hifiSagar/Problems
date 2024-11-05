cb5e87dddfe65c96e70ab6c82f35e9d61079cdc0
SAGAR T <ssagu6661@gmail.com>
2024-11-05 19:13:15 +0530

    Add files via upload

---
+++ cprogram13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a = 8, b = 10, c = 10;
+    if(a > b)
+    {
+        printf("A is greater than B \n");
+    }  
+    else if(a > c)
+    {
+        printf("A is greater than C \n");
+    }
+    else {
+        printf("A is small  \n");
+    }
+ 
+    return 0;
+}
