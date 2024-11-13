59275fee507636a7ba510ce07dd79adc163075ef
SAGAR T <ssagu6661@gmail.com>
2024-11-13 18:39:59 +0530

    Add files via upload

---
+++ userinput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("input two numbers to add \n");
+    printf("enter first number \n");
+    scanf("%d ",&num1);
+    
+    printf("enter second number \n");
+         scanf("%d",&num2);
+          printf("Addition of two numbers is %d  \n",num1+num2);
+return 0;
+}
