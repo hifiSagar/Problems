c4993036b45a6925fe25b67c92a226d69f3570ca
SAGAR T <ssagu6661@gmail.com>
2024-12-01 08:17:57 +0530

    Add files via upload

---
+++ new5.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   char arr[10]={'s','a','g','a','r'};
+   printf("%s",arr);
+
+    return 0;
+}
